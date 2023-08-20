cccb43b77d95de0c4f3c7a2765db733a23319aff
carson2222 <olcraftyt@gmail.com>
2023-08-20 19:13:26 +0200

    last step to finish

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -1,27 +1,52 @@
+"use strict";
 const submit_btn = document.querySelector(".submit_btn");
+let valid_date = true;
 
 submit_btn.addEventListener("click", function () {
   const day = document.querySelector("#day");
   const month = document.querySelector("#month");
   const year = document.querySelector("#year");
   const inputs = [day, month, year];
-  const curDate = new Date();
+  const cur_date = new Date();
 
-  // Empty input error handling
+  // Clear errors from all inputs and assign error if is empty
   inputs.forEach((el) => {
+    clearErrors(el);
     if (!el.value) validChecker(el, "This field is required");
   });
 
   // Invalid input value error handling
-  if (+month.value > 0 || +month.value < 12)
+  if (+month.value < 0 || +month.value > 12)
     validChecker(month, "Must be a valid month");
-  if (+day.value > 0 || +day.value < 31 /* TODO: change to valid month days*/)
+  if (+day.value < 0 || +day.value > 31 /* TODO: change to valid month days*/)
     validChecker(day, "Must be a valid day");
-  if (+year.value > curDate.getFullYear())
+  if (+year.value > cur_date.getFullYear())
     validChecker(year, "Must be in the past");
 
-  // console.log(new Date(+year.value, +month.value, +day.value));
-  console.log(new Date(2023, 8, 33).getDay());
+  // Return whole function if the inputs are empty or not valid
+  if (!valid_date) return;
+
+  const input_date = new Date(+year.value, +month.value - 1, +day.value);
+  if (input_date.getDate() !== +day.value) {
+    validChecker(day, "Must be a valid date");
+    validChecker(month);
+    validChecker(year);
+  }
+
+  // Return whole function if the date is not valid
+  if (!valid_date) return;
+
+  // Display age
+  console.log(cur_date, input_date);
+  console.log((cur_date.getTime() - input_date.getTime()) / 1000);
+  console.log(new Date(cur_date - input_date));
+
+  console.log(secondsToYMD((cur_date.getTime() - input_date.getTime()) / 1000));
+  // ageDisplayer(
+  //   input_date.getFullYear(),
+  //   input_date.getMonth(),
+  //   input_date.getDate()
+  // );
 });
 
 /*       <div class="calculator--form__element">
@@ -29,12 +54,19 @@ submit_btn.addEventListener("click", function () {
           <input type="number" id="day" placeholder="DD" class="error-input" />
           <p class="error-display">This field is required</p>
         </div> */
-
+function clearErrors(input) {
+  const parent = input.closest(".calculator--form__element");
+  input.classList.remove("error-input");
+  parent.querySelector("label").classList.remove("error-label");
+  hideErrorMessage(parent.querySelector(".error-display"));
+}
 function validChecker(input, message) {
   const parent = input.closest(".calculator--form__element");
+  valid_date = false;
   addErrorClasses(parent, ["label", "input"]);
-  console.log(parent.querySelector(".error-display"));
-  displayErrorMessage(parent.querySelector(".error-display"), message);
+  if (message) {
+    displayErrorMessage(parent.querySelector(".error-display"), message);
+  }
 }
 
 function addErrorClasses(form_el, arr) {
@@ -51,4 +83,33 @@ function displayErrorMessage(element, message = "error") {
   if (element.classList.contains("hidden")) {
     element.classList.remove("hidden");
   }
+}
+function hideErrorMessage(element) {
+  if (!element.classList.contains("hidden")) {
+    element.classList.add("hidden");
+  }
+}
+function ageDisplayer(years, months, days) {
+  document.querySelector(".calculator--displayer__years").innerHTML = years;
+  document.querySelector(".calculator--displayer__months").innerHTML = months;
+  document.querySelector(".calculator--displayer__days").innerHTML = days;
+}
+function secondsToYMD(sec) {
+  const YMD = [0, 0, 0];
+  sec = Math.floor(sec);
+  var day = 60 * 60 * 24;
+
+  while (sec >= day * 365) {
+    sec - day * 365;
+    ++YMD[0];
+  }
+  while (sec >= day * 31) {
+    sec - day * 31;
+    ++YMD[1];
+  }
+  while (sec >= day) {
+    sec - day;
+    ++YMD[2];
+  }
+  return YMD;
 }
